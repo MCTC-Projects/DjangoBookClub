490ce605c6bd6fb41e951c3a0b99fc01929e5be4
boydjohnson <johnson.boyd@gmail.com>
2015-03-05 22:07:33 -0600

    Initial Django directory. Python manage.py syncdb has not been run so there is no DB

---
+++ bookit/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render_to_response,HttpResponse
+from django.views.generic import TemplateView
+
+
+
+class MainLogin(TemplateView):
+    def get(self, request, *args, **kwargs):
+
+
+
+    def post(self,request, *args, **kwargs):
