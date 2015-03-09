8ac23e03f040a5f6e6c08ecaa5183b1c8891502f
boydjohnson <johnson.boyd@gmail.com>
2015-03-09 14:27:02 -0500

    Apparently migrations need to be done locally and then added to Heroku

---
+++ bookit/migrations/0001_initial.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=50)),
+                ('author', models.CharField(max_length=20)),
+                ('isbn', models.IntegerField()),
+                ('description', models.TextField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='BookClub',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('bookclub_name', models.CharField(max_length=20)),
+                ('bookclub_description', models.TextField()),
+                ('owners_first_name', models.CharField(max_length=20)),
+                ('owners_email_address', models.EmailField(max_length=75)),
+                ('owners_password', models.CharField(max_length=30)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Review',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('review', models.TextField()),
+                ('rating', models.IntegerField()),
+                ('book', models.ForeignKey(to='bookit.Book')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=20)),
+                ('email_address', models.EmailField(max_length=75)),
+                ('password', models.CharField(max_length=15)),
+                ('bookclub', models.ManyToManyField(to='bookit.BookClub')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='review',
+            name='user',
+            field=models.ForeignKey(to='bookit.User'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='book',
+            name='bookclub',
+            field=models.ForeignKey(to='bookit.BookClub'),
+            preserve_default=True,
+        ),
+    ]
