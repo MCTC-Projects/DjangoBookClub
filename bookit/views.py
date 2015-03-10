ea1314e3afd4137c64cd1497364d26cb69dde6f9
boydjohnson <johnson.boyd@gmail.com>
2015-03-09 21:07:01 -0500

    Small changes to get from unicode to bytes

--- bookit/views.py
+++ bookit/views.py
@@ -40,7 +40,7 @@ class Instructions(TemplateView):
 class AppLogin(TemplateView):
     def get(self, request, *args, **kwargs):
         data = request.GET.get('data',b'')
-        data = str.encode(data)
+        data = str.encode(str(data))
         data_decoded = urlsafe_b64decode(data).decode('utf-8')
         data_dict = loads(data_decoded)
         if len(BookClub.objects.filter(owners_email_address=data_dict['email']))==0:
