3a547df67f95a14f1347bf2b9dfeb3733d808e80
boydjohnson <johnson.boyd@gmail.com>
2015-03-09 19:25:49 -0500

    added Bootstrap to registration.html and added 2nd password input form

--- bookit/BookForms.py
+++ bookit/BookForms.py
@@ -6,6 +6,7 @@ from bookit.models import Book, BookClub, Review, User
 
 class BookClubRegistration(ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
+    password_again = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = BookClub
         fields = ('bookclub_name','bookclub_description','owners_first_name','owners_email_address')
