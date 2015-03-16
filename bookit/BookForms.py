82df12e3977cadfd708761aa475f0855d515030b
boydjohnson <johnson.boyd@gmail.com>
2015-03-16 17:45:44 -0500

    Will have to figure out how to make a form based on the User model

--- bookit/BookForms.py
+++ bookit/BookForms.py
@@ -3,13 +3,6 @@ from django import forms
 from bookit.models import Book, BookClub, Review
 
 
-class BookClubRegistration(ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    password_again = forms.CharField(widget=forms.PasswordInput)
-    class Meta:
-        model = BookClub
-        fields = ('bookclub_name','bookclub_description','owners_first_name','owners_email_address')
-
 
 
 
