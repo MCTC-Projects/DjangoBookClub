b8b231edc97d2ba945f889eb6283396ee6795626
boydjohnson <johnson.boyd@gmail.com>
2015-03-16 17:57:21 -0500

    May have fixed the BookClubRegistration form

--- bookit/BookForms.py
+++ bookit/BookForms.py
@@ -3,6 +3,13 @@ from django import forms
 from bookit.models import Book, BookClub, Review
 
 
+class BookClubRegistration(ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_again = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = BookClub
+        fields = ('bookclub_name', 'bookclub_description', 'owner')
+
 
 
 
