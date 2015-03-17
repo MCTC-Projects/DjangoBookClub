36118f7c06ab211ce3b6ec61b0c616e5a156c5b2
boydjohnson <johnson.boyd@gmail.com>
2015-03-16 20:23:44 -0500

    Fixing the registration form

--- bookit/BookForms.py
+++ bookit/BookForms.py
@@ -1,9 +1,9 @@
 from django.forms import ModelForm
 from django import forms
-from registration.forms import RegistrationFormUniqueEmail
+from django.contrib.auth.forms import UserCreationForm
 
 
-class BookClubRegistration(RegistrationFormUniqueEmail):
+class BookClubRegistration(UserCreationForm):
     bookclub_name = forms.CharField(max_length=20,widget=forms.TextInput(attrs={'class':'form-control'}))
     bookclub_description = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}))
 
