9da530ed41c9303430b853ce0b19b1480bf3bcf6
boydjohnson <johnson.boyd@gmail.com>
2015-03-16 20:12:51 -0500

    Fixing the registration form

--- bookit/BookForms.py
+++ bookit/BookForms.py
@@ -5,7 +5,7 @@ from registration.forms import RegistrationFormUniqueEmail
 
 class BookClubRegistration(RegistrationFormUniqueEmail):
     bookclub_name = forms.CharField(max_length=20,widget=forms.TextInput(attrs={'class':'form-control'}))
-    bookclub_description = forms.TextInput(widget=forms.Textarea(attrs={'class':'form-control'}))
+    bookclub_description = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}))
 
 
 
