e5476957c4478580029e2d4409c06caaeb1e16b3
boydjohnson <johnson.boyd@gmail.com>
2015-03-19 16:22:33 -0500

    Change to the BookClubRegistration form

--- bookit/BookForms.py
+++ bookit/BookForms.py
@@ -2,14 +2,16 @@ from django.forms import ModelForm
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
+from bookit.models import BookClub
 
 class BookClubRegistration(UserCreationForm):
     bookclub_name = forms.CharField(max_length=20,widget=forms.TextInput(attrs={'class':'form-control'}))
     bookclub_description = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}))
 
-
-
-
+    class Meta:
+        model = BookClub
+        labels = {'username': _('email'),
+                  }
 
 
 
