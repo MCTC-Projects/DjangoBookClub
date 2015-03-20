644dea8287f54b772c200dc02c6646a5117c1d15
boydjohnson <johnson.boyd@gmail.com>
2015-03-20 11:57:53 -0500

    Created the save() function definition to get the name and description information to BookClub

--- bookit/BookForms.py
+++ bookit/BookForms.py
@@ -8,10 +8,12 @@ class BookClubRegistration(UserCreationForm):
     bookclub_name = forms.CharField(max_length=20,widget=forms.TextInput(attrs={'class':'form-control'}))
     bookclub_description = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}))
 
-    class Meta:
-        labels = {'username': ('email',),
-                  }
-
+    def save(self, commit=True):
+        user = super(UserCreationForm,self).save(commit=True)
+        bc = BookClub(owner = user,bookclub_name = self.cleaned_data['bookclub_name'],bookclub_description = self.cleaned_data['bookclub_description'])
+        if commit:
+            bc.save()
+        return bc
 
 
 
